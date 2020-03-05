7af69907bc796cf5f327926b3100d531c856275a
sebas-git <javier_futbol97@hotmail.com>
2020-03-05 00:23:41 -0500

    camara

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+navigator.mediaDevices.getUserMedia({audio:true,video: true}).then((stream)=>{
+  consola.log(stream)
+
+  let video = document.getElementById('video')
+  video.srcObject= stream
+
+}).catch((err)=>console.log(err))
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
+
+
